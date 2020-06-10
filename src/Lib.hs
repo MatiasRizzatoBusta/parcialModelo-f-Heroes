a15f9a8a3f1581c671c3827c9bb76e2e5d360f04
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-10 18:35:04 -0300

    encontrarArtefacto

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,24 +8,44 @@ data Heroe = UnHeroe{
     epiteto :: String,
     reconocimiento :: Int,
     artefactos :: [Artefacto],
-    tareasRealizadas :: [Tarea]
+    tareasRealizadas :: [String]
 }deriving (Show)
 
 data Artefacto = UnArtefacto{
     rareza :: Int
 }deriving (Show,Eq)
 
-type Tarea = Heroe->Heroe
-
 --------------------------------------------- Punto 2 ---------------------------------------------
 pasarALaHistoria :: Heroe->Heroe
 pasarALaHistoria heroe |(cumpleCondicion (>1000).reconocimiento ) heroe = heroe{epiteto = "el mitico"}
-                       |(cumpleCondicion (<500).reconocimiento) heroe = heroe{epiteto = "el magnifico",artefactos = lanzaDelOlimpo:(artefactos heroe)}
+                       |(cumpleCondicion (<500).reconocimiento) heroe = heroe{epiteto = "el magnifico",artefactos = agregoArtefacto lanzaDelOlimpo heroe}
                        |(cumpleCondicion (>100).reconocimiento) heroe = heroe{epiteto = "hoplita",artefactos=xiphos:(artefactos heroe)}
                        |otherwise = heroe
 
 cumpleCondicion :: (Int->Bool)->Int->Bool
 cumpleCondicion condicion reconocimiento = condicion reconocimiento
 
+agregoArtefacto :: Artefacto->Heroe->[Artefacto]
+agregoArtefacto artefacto heroe = artefacto:(artefactos heroe)
+
 lanzaDelOlimpo = UnArtefacto 100
 xiphos = UnArtefacto 50
+
+--------------------------------------------- Punto 3 ---------------------------------------------
+type Tarea = Heroe->Heroe
+
+encontrarArtefacto :: Artefacto->Tarea
+encontrarArtefacto artefacto heroe = heroe{reconocimiento = sumoReconocimiento (rareza artefacto) heroe,artefactos = agregoArtefacto artefacto heroe,tareasRealizadas = agregoTarea "encontrarArtefacto" heroe}
+
+sumoReconocimiento :: Int->Heroe->Int
+sumoReconocimiento rarezaArtefacto heroe = rarezaArtefacto + (reconocimiento heroe)
+
+agregoTarea :: String->Heroe->[String]
+agregoTarea tarea heroe = tarea:(tareasRealizadas heroe)
+
+
+
+
+
+
+
