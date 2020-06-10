0c933f52f40d0a1ff411903111c1c4abdd20dfb8
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-10 18:27:32 -0300

    punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,31 @@
 module Lib where
 import Text.Show.Functions
-
 laVerdad = True
+
+--------------------------------------------- Punto 1 ---------------------------------------------
+
+data Heroe = UnHeroe{
+    epiteto :: String,
+    reconocimiento :: Int,
+    artefactos :: [Artefacto],
+    tareasRealizadas :: [Tarea]
+}deriving (Show)
+
+data Artefacto = UnArtefacto{
+    rareza :: Int
+}deriving (Show,Eq)
+
+type Tarea = Heroe->Heroe
+
+--------------------------------------------- Punto 2 ---------------------------------------------
+pasarALaHistoria :: Heroe->Heroe
+pasarALaHistoria heroe |(cumpleCondicion (>1000).reconocimiento ) heroe = heroe{epiteto = "el mitico"}
+                       |(cumpleCondicion (<500).reconocimiento) heroe = heroe{epiteto = "el magnifico",artefactos = lanzaDelOlimpo:(artefactos heroe)}
+                       |(cumpleCondicion (>100).reconocimiento) heroe = heroe{epiteto = "hoplita",artefactos=xiphos:(artefactos heroe)}
+                       |otherwise = heroe
+
+cumpleCondicion :: (Int->Bool)->Int->Bool
+cumpleCondicion condicion reconocimiento = condicion reconocimiento
+
+lanzaDelOlimpo = UnArtefacto 100
+xiphos = UnArtefacto 50
