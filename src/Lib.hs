d2cfdcbd26ec2975551a34988870ff789ee2f522
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-06-10 19:05:17 -0300

    pto 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -51,5 +51,31 @@ triplicoRareza heroe = map tomoRareza (artefactos heroe)
 
 tomoRareza :: Artefacto->Artefacto
 tomoRareza artefacto = artefacto{rareza = (*3) (rareza artefacto)}
+{-
+ayudarACruzarLaCalle :: Int->Tarea
+ayudarACruzarLaCalle cuadrazCrusadas heroe = heroe{epiteto = queTanGroso cuadrazCrusadas,tareasRealizadas = agregoTarea "ayudar a cruzar la calle" heroe }
+
+queTanGroso :: Int->String
+queTanGroso cuadrazCrusadas |cuadrazCrusadas >1=  queTanGroso (cuadrazCrusadas -1) :("gros" ++ "o")
+                            |otherwise = "groso"
+
+-}
+
+data Bestia = UnaBestia{
+    nombre :: String,
+    debilidad :: AprovechaDebilidad
+}deriving (Show)
+
+type AprovechaDebilidad = Heroe->Bool
+
+matarBestia :: Bestia->Tarea
+matarBestia bestia heroe |explotaDebilidad (debilidad bestia) heroe = heroe{epiteto= tituloDeAsesino (nombre bestia),tareasRealizadas = agregoTarea "matar Bestia" heroe}
+                         |otherwise =heroe{epiteto="el cobarde",artefactos = tail (artefactos heroe)}
+
+explotaDebilidad :: AprovechaDebilidad->Heroe->Bool
+explotaDebilidad debilidadBestia heroe = debilidadBestia heroe --la debilidad es una funcion que toma al heroe pq esta puede ser
+--cualquier cosa que tenga el heroe y como no se que puede ser, hago que el bicho lo sepa y lo tenga codeado el 
 
 
+tituloDeAsesino :: String->String
+tituloDeAsesino nombreBestia = "el asesino de " ++ nombreBestia
